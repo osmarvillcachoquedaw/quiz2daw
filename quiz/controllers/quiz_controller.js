a0b1ccd914364959bab5c11ba1e0a8162c8d17ea
alexBaquerizo <elbatracio_21@hotmail.com>
2015-10-19 13:51:52 +0200

    Crear pregunta

--- quiz/controllers/quiz_controller.js
+++ quiz/controllers/quiz_controller.js
@@ -39,4 +39,22 @@ exports.index = function(req, res) {
 	).catch(function(error){next(error);})
 };
 
+// GET /quizes/new
+exports.new = function(req, res) {
+	var quiz = models.Quiz.build(
+		{pregunta: "Pregunta", respuesta: "Respuesta"}
+		);
+
+		res.render('quizes/new', {quiz: quiz});
+};
+
+// POST /quizes/create
+exports.create = function(req, res) {
+	var quiz = models.Quiz.build( req.body.quiz );
+	
+	quiz.save({fields: ["pregunta","respuesta"]}).then(function(){
+		res.redirect('/quizes');	
+	})
+};
+
 
