117245da6003b02e8c9969bd0813dfede1d15ebc
alexBaquerizo <elbatracio_21@hotmail.com>
2015-10-21 12:16:01 +0200

    funcionalidad edit

--- quiz/controllers/quiz_controller.js
+++ quiz/controllers/quiz_controller.js
@@ -68,4 +68,29 @@ exports.create = function(req, res) {
 	);
 };
 
+// GET /quizes/:id/edit
+exports.edit = function(req, res) {
+	var quiz = req.quiz; // autoload de instacia de quiz
+		res.render('quizes/edit', {quiz: req.quiz});
+};
 
+
+//PUT /quizes/:id
+exports.update = function(req, res) {
+		req.quiz.pregunta = req.body.quiz.pregunta;
+		req.quiz.respuesta = req.body.quiz.respuesta;
+		
+		req.quiz
+		.validate()
+		.then(
+		function(err){
+			if(err){
+			res.render('quizes/edit',{quiz: req.quiz, errors: err.errors});
+			}else{
+				req.quiz
+				.save({fields:["pregunta","respuesta"]})
+				.then(function(){ res.redirect('/quizes');});
+				}
+			}
+		);
+};
