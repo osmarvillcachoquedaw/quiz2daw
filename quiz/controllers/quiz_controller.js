dca80adf2b7ea7549d0ac53f825c4073f7f83b8b
Alberto Sierra <alberto.sierra@murciaeduca.es>
2015-10-22 16:28:14 +0200

    Editar preguntas

--- quiz/controllers/quiz_controller.js
+++ quiz/controllers/quiz_controller.js
@@ -22,7 +22,7 @@ exports.index = function(req, res) {
 // GET /quizes/:quizId
 exports.show = function(req, res) {
     res.render('quizes/show', {quiz: req.quiz});
-}
+};
 
 // GET /quizes/answer
 exports.answer = function(req, res) {
@@ -53,6 +53,31 @@ exports.create = function(req, res) {
 			res.render('quizes/new', {quiz: quiz, errors: err.errors});
 			} else {
 				quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+					res.redirect('/quizes');
+				})	//Redireccion HTTP (URL relativo) lista de preguntas
+			}
+		}
+	);
+};
+
+// GET /quizes/:quizId/edit
+exports.edit = function(req, res) {
+	var quiz = req.quiz; //autoload de instancia de quiz
+    res.render('quizes/edit', {quiz: quiz});
+};
+
+exports.update = function(req, res) {
+	req.quiz.pregunta = req.body.quiz.pregunta;
+	req.quiz.respuesta = req.body.quiz.respuesta;
+	
+	//guarda en DB los campos pregunta y respuesta de quiz
+	req.quiz.validate()
+	.then(
+		function(err){
+			if(err) {
+			res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+			} else {
+				req.quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
 					res.redirect('/quizes');
 				})	//Redireccion HTTP (URL relativo) lista de preguntas
 			}
