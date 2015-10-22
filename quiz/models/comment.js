6a8dd386de007b38e2360fdd99ebccc4cea10927
alexBaquerizo <elbatracio_21@hotmail.com>
2015-10-22 11:07:18 +0200

    Añadir comentario

---
+++ quiz/models/comment.js
@@ -0,0 +1,13 @@
+// Definicion del modelo Comment con validación
+
+module.exports = function(sequelize, DataTypes){
+	return sequelize.define(
+		'Comment',
+		{ texto:{
+			type: DataTypes.STRING,
+			validate: {notEmpty: {msg: "-> Falta Comentario"}}
+			}
+		}
+	
+	);
+}
