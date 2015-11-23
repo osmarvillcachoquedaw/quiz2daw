0c9c5d19d2aafbaed3c3ada6a1dff172c99e894c
joycon <fika_sanjavier@hotmail.com>
2015-11-23 12:11:24 +0100

    ejercicio nº2

--- quiz/models/models.js
+++ quiz/models/models.js
@@ -36,6 +36,8 @@ CuestionarioAsignado.belongsTo(Cuestionario, Alumno);
 Alumno.hasMany(CuestionarioAsignado);
 Cuestionario.hasMany(CuestionarioAsignado);
 
+Alumno.belongsTo(Grupo, {foreignKey:'grupoAlumno'});
+
 // sequelize.sync() crea e inicializa tabla de preguntas en DB
 sequelize.sync().then(function() {
 	// then(..) ejecuta el manejador una vez creada la tabla
@@ -70,7 +72,8 @@ sequelize.sync().then(function() {
 						apellido2: 'López',
 						nombre: 'Juan',
 						email: 'Juan@gmail.com',
-						userId: 2
+						userId: 2,
+						grupoAlumno: ""
 		});
             };
 	});
