291dea0f409c388d60866f32969df1d3bb7948e3
unknown <berxini@hotmail.com>
2015-11-23 09:59:29 +0100

    ejercicionº1

--- quiz/app.js
+++ quiz/app.js
@@ -20,6 +20,7 @@ app.set('view engine', 'ejs');
 app.use(partials());
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logo(path.join(__dirname,'public','logo.png')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
